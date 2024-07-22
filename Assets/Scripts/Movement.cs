905a7ffbe904789f538c426769e98eebe5e7eca4
tjzwave <watcharin022003@gmail.com>
2024-07-23 01:09:28 +0700

    add

---
+++ Assets/Scripts/Movement.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Movement : MonoBehaviour
+{
+    public float speed;
+    public Animator animator;
+
+    private void Update()
+    {
+        float horizontal = Input.GetAxisRaw("Horizontal");
+        float vertical = Input.GetAxisRaw("Vertical");
+
+        Vector3 direction = new Vector3(horizontal, vertical, 0).normalized;
+
+        transform.position += direction * speed * Time.deltaTime;
+
+        AnimatorMovement(direction);
+    }
+
+    void AnimatorMovement(Vector3 direction)
+    {
+        if (animator != null)
+        {
+            if (direction.magnitude > 0)
+            {
+                animator.SetBool("IsMoving", true);
+                animator.SetFloat("Horizontal", direction.x);
+                animator.SetFloat("Vertical", direction.y);
+            }
+            else
+            {
+                animator.SetBool("IsMoving", false);
+            }
+        }
+    }
+}
