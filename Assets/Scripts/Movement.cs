4b5ec2bcba7852e126785318b1b2ce038a00f7ac
tjzwave <watcharin022003@gmail.com>
2024-07-25 22:51:12 +0700

    Update Movement.cs

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,7 +2,7 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour //kuay rai wa
+public class Movement : MonoBehaviour 
 {
     public float speed;
     public Animator animator;
