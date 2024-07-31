20dd2afd1e0dda9c399f4d1385334fbb3705b1e7
Pluem-piyapong <k.55.zaza22@gmail.com>
2024-07-31 22:55:15 +0700

    pluem

--- Assets/Scripts/Movement.cs
+++ Assets/Scripts/Movement.cs
@@ -2,20 +2,32 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Movement : MonoBehaviour 
+public class Movement : MonoBehaviour
 {
-    public float speed;
-    public Animator animator;
+    public float speed = 2; // Speed of the character
+    public Animator animator; // Reference to the Animator component
+    public Vector3 direction; // Direction vector for movement
+    public VectorValue startingPosition; // Reference to the starting position
+
+    private void Start()
+    {
+        // Set the starting position from the VectorValue
+        transform.position = startingPosition.initialValue;
+    }
 
     private void Update()
     {
+        // Get input from the horizontal and vertical axes
         float horizontal = Input.GetAxisRaw("Horizontal");
         float vertical = Input.GetAxisRaw("Vertical");
 
-        Vector3 direction = new Vector3(horizontal, vertical, 0).normalized;
+        // Create a direction vector from the input
+        direction = new Vector3(horizontal, vertical, 0).normalized;
 
+        // Move the character based on the direction and speed
         transform.position += direction * speed * Time.deltaTime;
 
+        // Update the animator with the movement direction
         AnimatorMovement(direction);
     }
 
---
+++ Assets/VectorValue.cs
@@ -0,0 +1,7 @@
+using UnityEngine;
+
+[CreateAssetMenu(fileName = "New Vector Value", menuName = "ScriptableObjects/VectorValue")]
+public class VectorValue : ScriptableObject
+{
+    public Vector2 initialValue;
+}
