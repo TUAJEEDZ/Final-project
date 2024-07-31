6b29b256fb280bccf3f2ba604705895db1b7ac9b
jalee234 <b6536916@g.sut.ac.th>
2024-08-01 01:07:10 +0700

    test collect pumkin in inventory

--- Assets/Scripts/Collectable.cs
+++ Assets/Scripts/Collectable.cs
@@ -3,17 +3,21 @@ using System.Collections.Generic;
 using UnityEngine;
 public class Collectable : MonoBehaviour
 {
-	//player walks into collectable 
-	//add collectable to player
-	//delete collectable from the screen
+	public CollectableType type;
 	private void OnTriggerEnter2D (Collider2D collision)
 	{
+
 		Player player = collision.GetComponent<Player>();
 
 		if(player)
 		{
-			player.numPumkin++;
+			player.inventory.Add(type);
 			Destroy(this.gameObject);
 		}
 	}
+}
+
+public enum CollectableType
+{
+	NONE, pumkin
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -4,5 +4,10 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-   public int numPumkin = 0;
+   public Inventory inventory;
+
+   private void Awake()
+   {
+	   inventory = new Inventory(21);
+   }
 }
