217fa215fef02fe1c3055e5e44238e6db3323342
Pluem-piyapong <k.55.zaza22@gmail.com>
2024-08-15 20:20:49 +0700

    pluem

--- Assets/Scripts/LevelMove_Ref.cs
+++ Assets/Scripts/LevelMove_Ref.cs
@@ -2,17 +2,48 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class LevelMove_Ref : MonoBehaviour
 {
     public int sceneBuildIndex;
+    public Text interactionText; // Drag your UI Text component here in the Inspector
+    public Image interactionImage; // Drag your UI Image component here in the Inspector
+    private bool playerInTrigger = false;
+
+    private void Start()
+    {
+        // Hide the text and image at the start
+        interactionText.gameObject.SetActive(false);
+        interactionImage.gameObject.SetActive(false);
+    }
 
     private void OnTriggerEnter2D(Collider2D other)
     {
-        Debug.Log("Trigger Entered");
         if (other.CompareTag("Player"))
         {
-            Debug.Log("Switching Scene to " + sceneBuildIndex);
+            playerInTrigger = true;
+            interactionText.gameObject.SetActive(true); // Show the text when player enters trigger area
+            interactionImage.gameObject.SetActive(true); // Show the image when player enters trigger area
+
+          
+        }
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerInTrigger = false;
+            interactionText.gameObject.SetActive(false); // Hide the text when player exits trigger area
+            interactionImage.gameObject.SetActive(false); // Hide the image when player exits trigger area
+        }
+    }
+
+    private void Update()
+    {
+        if (playerInTrigger && Input.GetKeyDown(KeyCode.F))
+        {
             SceneManager.LoadScene(sceneBuildIndex, LoadSceneMode.Single);
         }
     }
